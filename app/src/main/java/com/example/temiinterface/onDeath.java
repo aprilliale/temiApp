b38c833a271c6c13306e2529570c5bcdb0fb673e
aprilliale <aprillia.le@gmail.com>
2022-12-23 00:17:42 +0900

    third commit

---
+++ app/src/main/java/com/example/temiinterface/onDeath.java
@@ -0,0 +1,15 @@
+package com.example.temiinterface;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class onDeath extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_death);
+    }
+
+}
