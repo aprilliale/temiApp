c9821daa28ddef483e97392e0c70785b4422d53e
aprilliale <aprillia.le@gmail.com>
2022-12-22 23:44:44 +0900

    second commit

--- app/src/main/java/com/example/temiinterface/MainScreen.java
+++ app/src/main/java/com/example/temiinterface/MainScreen.java
@@ -2,21 +2,15 @@ package com.example.temiinterface;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        new Handler().postDelayed(new Runnable() {
-            @Override public void run() {
-                Intent i = new Intent(MainActivity.this, MainScreen.class); startActivity(i);
-                finish(); } }, 3000);
+        setContentView(R.layout.activity_main_screen);
     }
 
     @Override
